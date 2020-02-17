965b316e9fd0f8a9f53b5486162c00669e01d240
dgu-pmos <gudah1478@dongguk.edu>
2020-02-17 17:55:49 +0900

    edit admin put route

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ const APP_KEY = process.env.APP_KEY;
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "token, Authorization, Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 app.set('trust proxy', true);
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@ const Channel = require('../models/channel');
 const User = require('../models/user');
 const authUtil = require('../module/authUtil');
 const missParameters = require('../module/missParameters');
+const axios = require('axios');
 const moment = require('moment');
 require('moment-timezone');
 moment.tz.setDefault("Asia/Seoul");
 require("moment-duration-format");
-
+require('dotenv').config();
 router.use('/auth', require('./auth'));
+const APP_KEY = process.env.APP_KEY;
 
 /*
 const cateCode = require('../module/cateCode');
@@ -26,7 +28,6 @@ router.get('/:sort/:category/:keyword/:page', async (req, res) => {
         createdAt: {"$gte": moment().format('YYYY-MM-DD')}
     };
     let sort;
-    
     if(req.params.category !== '0'){
         condition.category = req.params.category;
     }
@@ -38,21 +39,40 @@ router.get('/:sort/:category/:keyword/:page', async (req, res) => {
     {
         sort = [[req.params.sort, 'descending']];
     }
-    let result = await Channel
+    const result = await Channel
     .find(condition)
     .sort(sort)
     .skip((10 * req.params.page) - 10)
     .limit(10);
-    result.push({maxPage : maxPage});
-
-    return res.status(200).send(result);
+    return res.status(200).send({
+        result,
+        maxPage,
+    });
 });
 
 router.get('/youtuber/:id', async (req, res) => {
-    let result = await Channel
+    const result = await Channel
     .find({id: req.params.id})
     .sort([['createdAt', 'descending']]);
-    return res.status(200).send(result);
+    const max_view = await Video
+    .findOne({channel: result[0]._id, createdAt: {"$gte": moment().format('YYYY-MM-DD')}}, null, {sort: {viewCount: -1 }})
+    .select('viewCount');
+    const max_like = await Video
+    .findOne({channel: result[0]._id, createdAt: {"$gte": moment().format('YYYY-MM-DD')}}, null, {sort: {likeCount: -1 }})
+    .select('likeCount');
+    const max_dislike = await Video
+    .findOne({channel: result[0]._id, createdAt: {"$gte": moment().format('YYYY-MM-DD')}}, null, {sort: {dislikeCount: -1 }})
+    .select('dislikeCount');
+    const max_comment = await Video
+    .findOne({channel: result[0]._id, createdAt: {"$gte": moment().format('YYYY-MM-DD')}}, null, {sort: {commentCount: -1 }})
+    .select('commentCount');
+    return res.status(200).send({
+            result,
+            max_view: max_view.viewCount,
+            max_like: max_like.likeCount,
+            max_dislike: max_dislike.dislikeCount,
+            max_comment: max_comment.commentCount,
+        });
 });
 
 router.get('/admin/:page', authUtil.validToken, async (req, res) => {
@@ -62,8 +82,10 @@ router.get('/admin/:page', authUtil.validToken, async (req, res) => {
     .sort([['createdAt', 'descending']])
     .skip((10 * req.params.page) - 10)
     .limit(10);
-    result.push({maxPage : maxPage});
-    return res.status(200).send(result);
+    return res.status(200).send({
+        result,
+        maxPage,
+    });
 });
 
 router.post('/admin', authUtil.validToken, async (req, res) => {
@@ -85,6 +107,16 @@ router.post('/admin', authUtil.validToken, async (req, res) => {
     if(exUser.length != 0) {
         return res.status(404).send('exist youtuber info');
     }
+    const checkId = await axios.get('https://www.googleapis.com/youtube/v3/channels', {
+        params: {
+            key : APP_KEY,
+            part: "id",
+            id: json.id,
+        }
+    });
+    if(checkId.data.items.length == 0) {
+        return res.status(404).send('isn\'t correct youtuber id');
+    }
     let user = new User({
         id: json.id,
         name: json.name,
@@ -100,10 +132,6 @@ router.put('/admin/:id', authUtil.validToken, async (req, res) => {
     let json = {};
     json.name = req.body.name;
     json.category = req.body.category;
-    if(json.pw) {
-        const salt = bcrypt.genSaltSync(10);
-        json.pw = bcrypt.hashSync(json.pw, salt);
-    }
     await User.updateOne({id: req.params.id}, json);
     return res.status(200).send('edit success');
 });
